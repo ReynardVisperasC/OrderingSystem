38f1c8897f1b917c4200328a7e7cdb9e87129a2a
ReynardVisperasC <115077042+ReynardVisperasC@users.noreply.github.com>
2025-04-23 09:24:45 +0800

    recreate the model category for file Structure

---
+++ model/CategoryModel.java
@@ -0,0 +1,34 @@
+package model;
+
+public class CategoryModel{
+
+    private static int idCounter = 0;
+    private int id;
+    private String categoryName;
+
+    //lets create a contructor use to initialize value meaning it before running the program the initial values of the variables below is zero
+    public CategoryModel(String categoryName){
+        this.id = ++idCounter;
+        this.categoryName = categoryName;
+    }
+    //getters ito ung kukuha ng value outside 
+    public int getId(){
+        return id;
+    }
+    public String getCategoryName(){
+        return categoryName;
+    }
+    public void  setCategoryName(String categoryName){
+        this.categoryName = categoryName;
+
+    }
+
+    @Override
+    public String toString(){
+        return "ID" + id + "Name" + categoryName;
+    }
+
+    public static void setIdCounter(int value){
+        idCounter = value;
+    }
+}
